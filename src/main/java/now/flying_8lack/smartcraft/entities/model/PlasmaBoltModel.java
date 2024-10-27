1c1750df621c60a1a7f689576b5cf3f601f61299
flying_8lack <megacryleon@gmail.com>
2024-10-27 23:53:36 +0300

    fixed textures for plasma projectile

--- src/main/java/now/flying_8lack/smartcraft/entities/model/PlasmaBoltModel.java
+++ src/main/java/now/flying_8lack/smartcraft/entities/model/PlasmaBoltModel.java
@@ -8,50 +8,64 @@ package now.flying_8lack.smartcraft.entities.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import cpw.mods.modlauncher.api.ITransformationService;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.client.model.renderable.IRenderable;
-import net.neoforged.neoforge.client.model.renderable.ITextureRenderTypeLookup;
 import now.flying_8lack.smartcraft.entities.projectiles.PlasmaBoltEntity;
+import org.jetbrains.annotations.NotNull;
 
 import static now.flying_8lack.smartcraft.main.SmartCraft.MOD_ID;
 
 public class PlasmaBoltModel<T extends PlasmaBoltEntity> extends EntityModel<T> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(MOD_ID, "plasma_bolt"), "main");
-    private final ModelPart body;
 
+
+    private final ModelPart body;
 
     public PlasmaBoltModel(ModelPart root) {
         this.body = root.getChild("body");
+        ModelPart dark = this.body.getChild("dark");
+        ModelPart light = this.body.getChild("light");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -4.0F, -1.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 17.0F, -2.0F));
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, -2.0F, 0.0F));
 
-        return LayerDefinition.create(meshdefinition, 16, 16);
+        PartDefinition dark = body.addOrReplaceChild("dark", CubeListBuilder.create().texOffs(8, 18).addBox(-1.0F, 8.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(1.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition light = body.addOrReplaceChild("light", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, 8.0F, -1.0F, 3.0F, 3.0F, 3.0F, new CubeDeformation(1.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition head_r1 = light.addOrReplaceChild("head_r1", CubeListBuilder.create().texOffs(12, 12).addBox(-1.5F, -1.5F, -3.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.5F, 9.5F, 0.5F, 0.0F, 0.0F, 0.7418F));
+
+        PartDefinition cube_z8_r1 = light.addOrReplaceChild("cube_z8_r1", CubeListBuilder.create().texOffs(12, 6).addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(1.0F)), PartPose.offsetAndRotation(-0.5F, 9.5F, 0.5F, 0.0F, 0.0F, 1.3963F));
+
+        PartDefinition cube_z6_r1 = light.addOrReplaceChild("cube_z6_r1", CubeListBuilder.create().texOffs(12, 0).addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(1.0F)), PartPose.offsetAndRotation(-0.5F, 9.5F, 0.5F, 0.0F, 0.0F, 1.0472F));
+
+        PartDefinition cube_z4_r1 = light.addOrReplaceChild("cube_z4_r1", CubeListBuilder.create().texOffs(0, 12).addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(1.0F)), PartPose.offsetAndRotation(-0.5F, 9.5F, 0.5F, 0.0F, 0.0F, 0.6981F));
+
+        PartDefinition cube_z2_r1 = light.addOrReplaceChild("cube_z2_r1", CubeListBuilder.create().texOffs(0, 6).addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(1.0F)), PartPose.offsetAndRotation(-0.5F, 9.5F, 0.5F, 0.0F, 0.0F, 0.3491F));
+
+        return LayerDefinition.create(meshdefinition, 32, 32);
+    }
+    @Override
+    public void setupAnim(@NotNull PlasmaBoltEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
     }
 
-    @Override
-    public void setupAnim(PlasmaBoltEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
-    }
-
 
 
     @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
+    public void renderToBuffer(@NotNull PoseStack poseStack, @NotNull VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
         body.render(poseStack, buffer, packedLight, packedOverlay, color);
     }
+
 
 
 
--- src/main/java/now/flying_8lack/smartcraft/entities/render/PlasmaBoltRender.java
+++ src/main/java/now/flying_8lack/smartcraft/entities/render/PlasmaBoltRender.java
@@ -1,39 +1,72 @@
 package now.flying_8lack.smartcraft.entities.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.*;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LightLayer;
 import now.flying_8lack.smartcraft.entities.model.PlasmaBoltModel;
 import now.flying_8lack.smartcraft.entities.projectiles.PlasmaBoltEntity;
+import org.jetbrains.annotations.NotNull;
 
 import static now.flying_8lack.smartcraft.main.SmartCraft.MOD_ID;
 
 public class PlasmaBoltRender extends EntityRenderer<PlasmaBoltEntity> {
-    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "plasma_bolt");
-    private final PlasmaBoltModel model;
+    public static final ResourceLocation TEXTURE =
+            ResourceLocation.fromNamespaceAndPath(MOD_ID,
+                    "textures/entity/plasma_bolt.png");
+    private final PlasmaBoltModel<PlasmaBoltEntity> model;
     public PlasmaBoltRender(EntityRendererProvider.Context context) {
         super(context);
 
-        this.model = new PlasmaBoltModel(context.bakeLayer(PlasmaBoltModel.LAYER_LOCATION));
+        this.model = new PlasmaBoltModel<>(context.bakeLayer(PlasmaBoltModel.LAYER_LOCATION));
     }
 
 
     @Override
-    public ResourceLocation getTextureLocation(PlasmaBoltEntity entity) {
+    public @NotNull ResourceLocation getTextureLocation(PlasmaBoltEntity entity) {
         return TEXTURE;
     }
 
     @Override
     public void render(PlasmaBoltEntity p_entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
-        super.render(p_entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+        //super.render(p_entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
         poseStack.pushPose();
+        poseStack.mulPose(Axis.YP.rotationDegrees(p_entity.getYRot()));
+        poseStack.mulPose(Axis.XP.rotationDegrees(p_entity.getXRot()));
+//        Minecraft.getInstance().getItemRenderer().renderStatic(new ItemStack(Items.IRON_AXE),
+//                ItemDisplayContext.NONE,
+//                15728880,
+//                OverlayTexture.NO_OVERLAY,
+//                poseStack,
+//                bufferSource,
+//                p_entity.level(),
+//                0);
+//        Minecraft.getInstance().getEntityRenderDispatcher().render(p_entity,
+//                p_entity.getX(),
+//                p_entity.getY(),
+//                p_entity.getZ(),
+//                p_entity.getYRot(),
+//                1,
+//                poseStack,
+//                bufferSource,
+//                15728880);
+ //       poseStack.translate(0, -1, 0);
+
         model.renderToBuffer(poseStack,
-                bufferSource.getBuffer(RenderType.entitySolid(TEXTURE)),
-                packedLight,
-                0);
+                bufferSource.getBuffer(RenderType.entityTranslucent(TEXTURE)),
+                15728880,
+                OverlayTexture.NO_OVERLAY);
 
         poseStack.popPose();
+        //15728740
     }
+    
+
 }
